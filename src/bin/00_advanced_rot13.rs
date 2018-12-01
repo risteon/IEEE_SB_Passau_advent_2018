1aa45ece3ca81055f96476ab81c80a2cee973331
Christoph Rist <christoph_bernd.rist@daimler.com>
2018-12-01 18:43:55 +0100

    Add test problem
    Branch: master

--- src/bin/00_advanced_rot13.rs
+++ src/bin/00_advanced_rot13.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+    let line_no_testcases = iterator.next().unwrap().unwrap();
+    let _n = line_no_testcases.parse::<u32>().unwrap();
+
+
+    for line in iterator {
+        let l = line.unwrap();
+        let input_vec: Vec<char> = l.chars().collect();
+
+        let mut translated_vec = Vec::new();
+        translated_vec.reserve(input_vec.len());
+        for c in input_vec {
+            assert!(c.is_ascii_lowercase());
+            let a = ((c as u8 + 13 - 'a' as u8) % 26 + 'a' as u8) as char;
+            translated_vec.push(a);
+        }
+        let translated_string: String = translated_vec.into_iter().collect();
+        println!("{}", translated_string);
+    }
+
+    Ok(())
 }
